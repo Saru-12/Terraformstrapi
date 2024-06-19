6a31f30e52708a21fd28281d9367846fd1161e88
Sarthak Tiwaskar <166800862+Saru-12@users.noreply.github.com>
2024-06-19 19:02:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,73 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-1" # specify your region
+}
+
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi_sg12"
+  description = "Allow HTTP, HTTPS, and Strapi ports"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Strapi"
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "strapi" {
-  ami           = "ami-0cf2b4e024cdb6960"  
-  instance_type = "t2.micro"
+  ami           = "ami-08012c0a9ee8e21c4" # specify your desired AMI
+  instance_type = "t2.micro"              # specify your desired instance type
+
+  security_groups = [aws_security_group.strapi_sg.name]
 
   tags = {
-    Name = "StrapiInstance"
+    Name = "StrapiServer"
   }
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt update
-              sudo apt install -y nodejs npm git
-              sudo npm install pm2@latest -g
-              git clone https://github.com/PearlThoughts-DevOps-Internship/strapi.git /srv/strapi
+              sudo apt-get update -y
+              sudo apt-get install -y nodejs npm git
+              sudo npm install -g pm2
+              sudo mkdir -p /srv/strapi
               cd /srv/strapi
-              npm install
-              npm run build
-              pm2 start npm --name "strapi" -- run start
+              sudo git clone https://github.com/strapi/strapi.git .
+              sudo npm install
+              pm2 start npm --name "strapi" -- start
               EOF
+
+  key_name = var.key_name
+}
+
+variable "key_name" {
+  description = "The name of the key pair to use for the instance"
+  type        = string
+}
+
+output "instance_public_ip" {
+  value = aws_instance.strapi.public_ip
 }
